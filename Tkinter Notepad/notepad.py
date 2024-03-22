30dcab3317d29df3d6d4be2b50d09bd6088597b0
Goutham Kurapati <gouthamkurapati@gmail.com>
2024-03-22 21:16:12 +0530

    Updated Notepad

--- Tkinter Notepad/notepad.py
+++ Tkinter Notepad/notepad.py
@@ -1,45 +1,39 @@
 import tkinter as tk
 from tkinter import filedialog, Text
 
-class Notepad(tk.Tk):
-    def __init__(self):
-        super().__init__()
+def open_file():
+    filename = filedialog.askopenfilename()
+    if filename:
+        with open(filename, 'r') as f:
+            content = f.read()
+            root.text_widget.delete('1.0', "end")
+            root.text_widget.insert('1.0', content)
 
-        self.title("Notepad")
-        self.geometry("600x400")
+def save_file():
+    filename = filedialog.asksaveasfilename()
+    if filename:
+        with open(filename, 'w') as f:
+            f.write(root.text_widget.get('1.0', 'end'))
 
-        menubar = tk.Menu(self)
-        file_menu = tk.Menu(menubar, tearoff=0)
+def new_file():
+    root.text_widget.delete('1.0', "end")
 
-        file_menu.add_command(label="New", command=self.new_file)
-        file_menu.add_command(label="Open", command=self.open_file)
-        file_menu.add_separator()
-        file_menu.add_command(label="Save", command=self.save_file)
-        file_menu.add_command(label="Exit", command=self.quit)
-        
-        menubar.add_cascade(label="File", menu=file_menu)
-        self.config(menu=menubar)
+root = tk.Tk()
 
-        self.text_widget = Text(self)
-        self.text_widget.pack(expand=True, fill='both')
+root.title("Notepad")
+root.geometry("600x400")
 
-    def new_file(self):
-        Notepad().mainloop()
+menubar = tk.Menu()
+file_menu = tk.Menu(menubar)
 
-    def open_file(self):
-        filename = filedialog.askopenfilename()
-        if filename:
-            with open(filename, 'r') as f:
-                content = f.read()
-                self.text_widget.delete('1.0', "end")
-                self.text_widget.insert('1.0', content)
+file_menu.add_command(label="New")
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_file)
+file_menu.add_command(label="Exit", command=root.quit)
 
-    def save_file(self):
-        filename = filedialog.asksaveasfilename()
-        if filename:
-            with open(filename, 'w') as f:
-                f.write(self.text_widget.get('1.0', 'end'))
+root.config(menu=file_menu)
 
-if __name__ == "__main__":
-    window = Notepad()
-    window.mainloop()
+root.text_widget = Text(root)
+root.text_widget.pack(expand=True, fill='both')
+
+root.mainloop()
